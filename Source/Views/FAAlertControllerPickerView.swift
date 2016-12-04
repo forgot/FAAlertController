27a2e3938205b23a2f559f0a261040518bb4eb61
Jesse Cox <jesse@apprhythmia.com>
2016-12-03 22:06:20 -0600

    Initial implementation of FAAlertControllerPickerView and Pickable protocol

---
+++ Source/Views/FAAlertControllerPickerView.swift
@@ -0,0 +1,91 @@
+//
+//  FAAlertControllerPickerView.swift
+//  FAAlertController
+//
+//  Created by Jesse Cox on 12/3/16.
+//  Copyright © 2016 Apprhythmia LLC. All rights reserved.
+//
+
+import UIKit
+
+
+/// Adopted by types used as items in an `FAAlertController` with the `.picker` presentation style
+public protocol Pickable {
+    /// The title to be displayed for the item
+    var title: String { get }
+    /// The subtitle to be displayed for the item
+    var subtitle: String? { get }
+}
+
+protocol FAAlertControllerPickerDelegate {
+    func didSelectItem(_ item: Pickable)
+}
+
+class FAAlertControllerPickerView: UITableView, UITableViewDataSource, UITableViewDelegate {
+
+    var items: [Pickable]?
+    
+    init(items: [Pickable]?) {
+        super.init(frame: .zero, style: .plain)
+        dataSource = self
+        delegate = self
+        self.items = items
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .white
+        heightAnchor.constraint(equalToConstant: 200).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+    // MARK: - Table view data source
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard section == 0 else {
+            fatalError("There should never be more than one section here.")
+        }
+        return items?.isEmpty == true ? 0 : items!.count
+    }
+    
+    
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "identifier")
+        // Configure the cell...
+        if let item = items?[indexPath.row] {
+            cell.textLabel?.text = item.title
+            cell.detailTextLabel?.text = item.subtitle
+        }
+        return cell
+     }
+    
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    // MARK: Table view delagate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let item = items?[indexPath.row], let pickerDelegate = FAAlertControllerAppearanceManager.sharedInstance.pickerDelegate {
+            pickerDelegate.didSelectItem(item)
+        }
+    }
+    
+}
